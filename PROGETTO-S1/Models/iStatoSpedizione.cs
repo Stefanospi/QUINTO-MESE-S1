5eb8c1c2180166b08a2f9e2a1c5e05de60a508a1
Stefanospi <sspitaleri26@gmail.com>
2024-07-16 16:07:23 +0200

    Progetto settimana 1

---
+++ PROGETTO-S1/Models/iStatoSpedizione.cs
@@ -0,0 +1,12 @@
+﻿namespace PROGETTO_S1.Models
+{
+    public interface iStatoSpedizione
+    {
+        public int Id { get; set; }
+        public int SpedizioneId { get; set; }
+        public string Stato { get; set; }
+        public string Luogo { get; set; }
+        public string Descrizione { get; set; }
+        public DateOnly DataOra { get; set; }
+    }
+}
