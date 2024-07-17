125415a6a18292004b209214378cc2eceb381f94
Stefanospi <sspitaleri26@gmail.com>
2024-07-17 23:45:11 +0200

    Popolamento Clienti

---
+++ PROGETTO-S1/Query/Popolamento Clienti.sql
@@ -0,0 +1,11 @@
+﻿-- Inserimento dati nella tabella ClientiAzienda
+INSERT INTO ClientiAzienda (Nome, Sede, Intestatario, PIVA) VALUES
+('Azienda Alpha', 'Roma', 'Mario Rossi', 'IT12345678901'),
+('Azienda Beta', 'Milano', 'Luca Bianchi', 'IT98765432109'),
+('Azienda Gamma', 'Torino', 'Anna Verdi', 'IT11121314151');
+
+-- Inserimento dati nella tabella ClientiPrivati
+INSERT INTO ClientiPrivato (Nome, Cognome, DataNascita, CF) VALUES
+('Giulia', 'Rossi', '1990-05-10', 'RSSGLI90E50H501S'),
+('Marco', 'Bianchi', '1985-11-23', 'BNCMRC85S23H501T'),
+('Luca', 'Verdi', '1978-02-15', 'VRDLUC78B15H501N');
