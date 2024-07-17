fa9e786705db5097cf5e010e09054453886d9a7c
Flavio-Converso <fla.converso@gmail.com>
2024-07-17 23:42:33 +0200

    .

---
+++ PROGETTO-S1/Models/StatoSpedizione.cs
@@ -0,0 +1,14 @@
+﻿namespace PROGETTO_S1.Models
+{
+    public class StatoSpedizione
+    {
+        public int IdStatoSpedizione { get; set; }
+        public string Stato { get; set; }
+        public string Luogo { get; set; }
+        public string Descrizione { get; set; }
+        public DateTime DataOraAggiornamento { get; set; }
+        public int FK_IdSpedizione { get; set; }
+
+        public virtual Spedizione Spedizione { get; set; }
+    }
+}
