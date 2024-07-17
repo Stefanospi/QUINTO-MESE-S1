85f833b7e2c63771d319ce08739a76ad37afe09b
Stefanospi <sspitaleri26@gmail.com>
2024-07-18 01:31:57 +0200

    Admin

--- PROGETTO-S1/Service/AdminService.cs
+++ PROGETTO-S1/Service/AdminService.cs
@@ -9,14 +9,20 @@ namespace PROGETTO_S1.Service
     public class AdminService : IAdminService
     {
         private readonly string _connectionString;
+
         private const string SPEDIZIONI_IN_CONSEGNA_COMMAND = @"
-SELECT s.IdSpedizione, s.FK_ClienteAzienda, s.FK_ClientePrivato, s.NumId, s.DataSpedizione, 
+            SELECT s.IdSpedizione, s.FK_ClienteAzienda, s.FK_ClientePrivato, s.NumId, s.DataSpedizione, 
                    s.Peso, s.CittaDestinatario, s.Indirizzo, s.NomeDestinatario, s.CostoSpedizione, 
                    s.DataConsegnaPrev
             FROM Spedizioni s
             JOIN StatoSpedizione st ON s.IdSpedizione = st.FK_IdSpedizione
             WHERE st.Stato = 'In Consegna'
             AND CONVERT(DATE, s.DataConsegnaPrev) = CAST(GETDATE() AS DATE);";
+
+        private const string TOTALE_SPEDIZIONI_NON_CONSEGNATE_COMMAND = @"
+        SELECT COUNT(*) FROM Spedizioni s
+        JOIN StatoSpedizione st ON s.IdSpedizione = st.FK_IdSpedizione
+        WHERE st.Stato != 'Consegnato';";
 
         public AdminService(IConfiguration configuration)
         {
@@ -56,6 +62,36 @@ SELECT s.IdSpedizione, s.FK_ClienteAzienda, s.FK_ClientePrivato, s.NumId, s.Data
                             return spedizioni;
                         }
                     }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public List<Spedizione> TotSpedizioniNonConsegnate()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(TOTALE_SPEDIZIONI_NON_CONSEGNATE_COMMAND, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            var spedizioni = new List<Spedizione>();
+                            while (reader.Read())
+                            {
+                                var spedizione = new Spedizione
+                                {
+
+                                };
+                            }
+                            return spedizioni;
+                        }
+                    }
+
                 }
             }
             catch (Exception ex)
